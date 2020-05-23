8ff323ca5c508af3df18c9f9628eef16e9749524
saksham695 <saksham695@gmail.com>
2020-05-24 03:23:39 +0530

    added todo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import Login from "./loginPage/Login";
 import CategoryComponent from "./categoryPage/category/CategoryComponent";
+
+//TODO 1:add prop types and default props
+//TODO 2:use loadash where needed
+//TODO 3:add comments , check naming , sort lines
 export default class App extends Component {
   constructor(props) {
     super(props);
