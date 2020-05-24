ca5b96fb1ccb7a9fdda3ac48461b748dc8787cda
saksham695 <saksham695@gmail.com>
2020-05-25 03:46:19 +0530

    added data

--- src/categoryPage/data/data.js
+++ src/categoryPage/data/data.js
@@ -1,64 +1,64 @@
 export const data = {
-  // categories: [
-  //   {
-  //     name: "Food",
-  //     items: [
-  //       {
-  //         id: "f3",
-  //         name: "Wheat",
-  //         description: "Used to make Roti",
-  //       },
-  //       {
-  //         id: "f4",
-  //         name: "Rice",
-  //         description: "Boil and eat",
-  //       },
-  //       {
-  //         id: "f5",
-  //         name: "Macroni",
-  //         description: "Boil Fry and eat",
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     name: "Electronics",
-  //     items: [
-  //       {
-  //         id: "e3",
-  //         name: "Mobile",
-  //         description: "Used to ruin your life",
-  //       },
-  //       {
-  //         id: "e4",
-  //         name: "Pen Drive",
-  //         description: "Used to store data",
-  //       },
-  //       {
-  //         id: "e5",
-  //         name: "Smart Watches",
-  //         description: "Track your activities",
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     name: "Books",
-  //     items: [
-  //       {
-  //         id: "b3",
-  //         name: "Learning React",
-  //         description: "Learn React",
-  //       },
-  //       {
-  //         id: "b4",
-  //         name: "React for Dummies",
-  //         description: "React in depth",
-  //       },
-  //       {
-  //         id: "b5",
-  //         name: "Objective C",
-  //         description: "Learn Objective C",
-  //       },
-  //     ],
-  //   },
-  // ],
+  categories: [
+    {
+      name: "Food",
+      items: [
+        {
+          id: "f3",
+          name: "Wheat",
+          description: "Used to make Roti",
+        },
+        {
+          id: "f4",
+          name: "Rice",
+          description: "Boil and eat",
+        },
+        {
+          id: "f5",
+          name: "Macroni",
+          description: "Boil Fry and eat",
+        },
+      ],
+    },
+    {
+      name: "Electronics",
+      items: [
+        {
+          id: "e3",
+          name: "Mobile",
+          description: "Used to ruin your life",
+        },
+        {
+          id: "e4",
+          name: "Pen Drive",
+          description: "Used to store data",
+        },
+        {
+          id: "e5",
+          name: "Smart Watches",
+          description: "Track your activities",
+        },
+      ],
+    },
+    {
+      name: "Books",
+      items: [
+        {
+          id: "b3",
+          name: "Learning React",
+          description: "Learn React",
+        },
+        {
+          id: "b4",
+          name: "React for Dummies",
+          description: "React in depth",
+        },
+        {
+          id: "b5",
+          name: "Objective C",
+          description: "Learn Objective C",
+        },
+      ],
+    },
+  ],
 };
